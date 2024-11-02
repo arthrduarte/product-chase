89170f80bdb45ac8e90afee7744628c62f25961f
Arthur Duarte <tuiduarte@gmail.com>
2024-11-02 12:39:22 -0400

    connect to mongodb

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,16 +1,33 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
+import mongoose from "mongoose";
 
 dotenv.config();
 
-
 const app: Express = express();
 const port = 3000;
+
+
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:3000`);
+app.get('/protected', (req, res) => {
+    res.send('Express + TypeScript Server');
 });
+
+const start = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@product-chase.ihtax.mongodb.net/product-chase`)
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
