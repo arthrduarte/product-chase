bc1a8bd2297bb141989272cf153d1a87bd2ece68
Arthur Duarte <tuiduarte@gmail.com>
2024-11-02 11:36:58 -0400

    remove nestjs

--- apps/api/dist/users/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './user.service';
-import { User } from './user.schema';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    findAll(): Promise<User[]>;
-    findByUsername(id: string): Promise<User | null>;
-    create(user: User): Promise<User>;
-}
