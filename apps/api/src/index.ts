3d9d2cab791dcbdb4acf3293e9b9fed20989e1e8
Arthur Duarte <tuiduarte@gmail.com>
2024-11-03 16:16:32 -0500

    add signup route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,33 +1,28 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { clerkMiddleware, requireAuth } from "@clerk/express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import signupRouter from './routes/signup.route';
 
 dotenv.config();
 
 const app: Express = express();
 const port = 3000;
 
+app.use(bodyParser.json());
 
-app.use(clerkMiddleware());
-
+app.use('/auth/signup', signupRouter);
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript Server'); 
 });
 
-app.get('/protected', (req, res) => {
-    res.send('Express + TypeScript Server');
-});
-
-const start = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@product-chase.ihtax.mongodb.net/product-chase`)
+mongoose
+    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@product-chase.ihtax.mongodb.net/product-chase`)
+    .then(() => {
         app.listen(port, () => {
-            console.log(`Server is running at http://localhost:${port}`);
+            console.log(`Server running on port ${port}`);
         });
-    } catch (error) {
+    })
+    .catch((error) => {
         console.log(error);
-    }
-}
-
-start();
+    });
