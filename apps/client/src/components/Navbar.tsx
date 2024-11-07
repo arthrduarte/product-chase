f359636b8175d79cdc040948bdf1f9f1b4adb536
Arthur Duarte <tuiduarte@gmail.com>
2024-11-06 21:45:46 -0500

    fix hydration problem caused by nested buttons

--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -19,28 +19,28 @@ export default function Component() {
                         <Link href="/" className="mx-2">Products</Link>
                         <Link href="/about" className="mx-2">About</Link>
                     </nav>
-                    <div className="hidden lg:flex">
+                    <div className="lg:flex">
                         <SignedOut>
-                            <Button>
-                                <SignInButton />
-                            </Button>
+                            <SignInButton>
+                                <Button>Sign In</Button>
+                            </SignInButton>
                         </SignedOut>
                         <SignedIn>
                             <UserButton />
                         </SignedIn>
                     </div>
-                </header>
-                <div onClick={() => setOpen(prev => !prev)} className="lg:hidden my-auto">
-                    <svg data-testid="geist-icon" height="16" strokeLinejoin="round" viewBox="0 0 16 16" width="16">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M1 2H1.75H14.25H15V3.5H14.25H1.75H1V2ZM1 12.5H1.75H14.25H15V14H14.25H1.75H1V12.5ZM1.75 7.25H1V8.75H1.75H14.25H15V7.25H14.25H1.75Z" fill="currentColor"></path>
-                    </svg>
-                </div>
-                {open && (
-                    <div className="absolute right-0 top-[60px] w-1/2 h-[calc(100vh-44px)] bg-white shadow-lg flex flex-col items-start p-4">
-                        <Link href="/">Products</Link>
-                        <Link href="/about">About</Link>
+                    <div onClick={() => setOpen(prev => !prev)} className="lg:hidden my-auto">
+                        <svg data-testid="geist-icon" height="16" strokeLinejoin="round" viewBox="0 0 16 16" width="16">
+                            <path fillRule="evenodd" clipRule="evenodd" d="M1 2H1.75H14.25H15V3.5H14.25H1.75H1V2ZM1 12.5H1.75H14.25H15V14H14.25H1.75H1V12.5ZM1.75 7.25H1V8.75H1.75H14.25H15V7.25H14.25H1.75Z" fill="currentColor"></path>
+                        </svg>
                     </div>
-                )}
+                    {open && (
+                        <div className="absolute right-0 top-[60px] w-1/2 h-[calc(100vh-44px)] bg-white shadow-lg flex flex-col items-start p-4">
+                            <Link href="/">Products</Link>
+                            <Link href="/about">About</Link>
+                        </div>
+                    )}
+                </header>
             </div>
         </>
     )
